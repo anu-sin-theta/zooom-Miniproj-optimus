577a04317cfcac956274346adaf4de2d7d6a1543
Anubhav Singh <audiq4456@gmail.com>
2025-01-29 18:32:19 +0530

    test

---
+++ zoomer/src/components/Navbar.tsx
@@ -0,0 +1,52 @@
+"use client"
+
+"use client";
+import React, { useEffect, useState } from "react";
+import { Menu, MenuItem } from "./ui/navbar-menu";
+import { cn } from "../../lib/utils";
+import SliderToggle from "./SliderToggle";
+import Link from "next/link";
+
+
+function Navbar({ className }: { className?: string }) {
+
+    const [active, setActive] = useState<string | null>(null);
+    const [selected, setSelected] = useState("light");
+  
+  useEffect(() => {
+      const savedTheme = localStorage.getItem('theme') || 'light';
+      setSelected(savedTheme);
+      document.body.className = savedTheme + '-mode';
+    }, [])
+
+    
+    
+        useEffect(() => {
+        document.body.className = selected;
+      }, [selected]);
+  return (
+    <div
+      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+    >
+      <Menu setActive={setActive}>
+        <MenuItem setActive={setActive} active={active} item="Home">
+        </MenuItem>
+
+        <MenuItem setActive={setActive} active={active} item="About">
+        </MenuItem>
+
+        <MenuItem setActive={setActive} active={active} item="Sign In">
+        </MenuItem>
+        <Link href="/dashboard">
+        
+        <MenuItem setActive={setActive} active={active} item="Dashboard">
+        </MenuItem>
+        </Link>
+        
+        <SliderToggle selected={selected} setSelected={setSelected} />
+      </Menu>
+    </div>
+  );
+}
+
+export default Navbar;
